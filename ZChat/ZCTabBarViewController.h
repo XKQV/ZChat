a2d12967749e4653903fb2fce078311084d0dc88
XKQ <407635142@qq.com>
2019-10-10 23:42:01 +0800

    first commit

---
+++ ZChat/ZCTabBarViewController.h
@@ -0,0 +1,17 @@
+//
+//  ZCTabBarViewController.h
+//  ZChat
+//
+//  Created by XKQ on 2019/10/10.
+//  Copyright © 2019 XKQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZCTabBarViewController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
